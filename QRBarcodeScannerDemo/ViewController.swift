807f1e1573f2bd0eddd12b6858ad62361f1acc37
Sergio Flores Ramírez <sergio.flores@axity.com>
2020-11-20 12:07:06 -0600

    Se realiza la configuración y programación inicial funcional

--- QRBarcodeScannerDemo/ViewController.swift
+++ QRBarcodeScannerDemo/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: -Outlets
+    @IBAction func openQRBCScannerAction() {
+        openQRBCScanner()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Functions
+    private func openQRBCScanner() {
+        let qrbcScannerVC = QRBCScannerViewController()
+        navigationController?.pushViewController(qrbcScannerVC, animated: true)
     }
 
 
