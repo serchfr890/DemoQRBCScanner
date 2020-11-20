e3d4adcf8cacd6317a0bff349939a643551fe221
Sergio Flores Ramírez <sergio.flores@axity.com>
2020-11-20 12:06:27 -0600

    Se realiza la configuración y programación inicial funcional

---
+++ QRBarcodeScannerDemo/QRBCScannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QRBCScannerViewController.swift
+//  QRBarcodeScannerDemo
+//
+//  Created by Sergio Flores Ramirez on 20/11/20.
+//
+
+import UIKit
+
+class QRBCScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
